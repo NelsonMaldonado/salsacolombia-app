393938535cba390452282a64cfd1d80dae98e0e8
Nelson Maldonado <play2nino@gmail.com>
2022-02-23 21:49:22 -0800

    prices added

--- src/components/prices.js
+++ src/components/prices.js
@@ -10,7 +10,7 @@ const Prices = () => {
           <h2>VIRTUAL OR IN-PERSON PRIVATE LESSONS</h2>
           <h3>1-on-1 LESSONS</h3>
           <h4>Single Lesson $65</h4>
-          <h5>5 Lesson Package $275</h5>
+          <h4>5 Lesson Package $275</h4>
           <p>
             One-on-One lessons are a great way to have a personal experience in
             a class of your choice. Start training like a pro with Nelly
@@ -20,16 +20,20 @@ const Prices = () => {
           </p>
         </DivThree>
         <DivFour>
-          <h2>VIRTUAL OR IN-PERSON PRIVATE LESSONS</h2>
-          <h3>1-on-1 LESSONS</h3>
-          <h4>Single Lesson $65</h4>
-          <h5>5 Lesson Package $275</h5>
+          <h2>
+            GROUP (BIRTHDAY / WEDDING / FAMILY / FRIENDS /
+            TECH-HEALTH-WORKSHOPS)
+          </h2>
+
+          <span>Livestream Package 1: $150 (Up to 10-people)</span>
+          <br />
+          <span>Livestream Package 2: $300 (Up to 10-people)</span>
+          <br />
+          <span>Livestream Package 3: 650 (Up to 50-people)</span>
           <p>
-            One-on-One lessons are a great way to have a personal experience in
-            a class of your choice. Start training like a pro with Nelly
-            Maldonado. Classes start from a basic diagnostic warm up, then dance
-            technique, steps, choreo modules and finally partner work. You get
-            to choose hours that work for you.{" "}
+            Enjoy a private dance group lesson exclusive to YOUR circle! This
+            package is great for birthday parties, team bonding and overall
+            group fun, social events, coorperate events, festivals and more!
           </p>
         </DivFour>
       </DivTwo>
@@ -39,6 +43,11 @@ const Prices = () => {
 export default Prices
 
 const TopDiv = styled.div`
+  font-family: poppins-extralight, poppins, sans-serif;
+  span {
+    font-weight: bold;
+    font-size: 12px;
+  }
   border: 5px solid red;
   height: 800px;
   display: flex;
@@ -56,12 +65,14 @@ const DivTwo = styled.div`
 `
 const DivThree = styled.div`
   border: 5px solid brown;
-  width: 100%;
+  width: 96%;
   height: 40vh;
+  padding-left: 10px;
 `
 
 const DivFour = styled.div`
   border: 5px solid purple;
-  width: 100%;
+  width: 96%;
   height: 40vh;
+  padding-left: 10px;
 `
