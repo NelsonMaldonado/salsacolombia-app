8e54e39be021b34d0f8772f924850f5c775fa8d7
Nelson Maldonado <play2nino@gmail.com>
2022-02-23 21:27:43 -0800

    prices component coming along very nice

--- src/components/prices.js
+++ src/components/prices.js
@@ -4,7 +4,35 @@ const Prices = () => {
   return (
     <TopDiv>
       <DivOne>Left box</DivOne>
-      <DivTwo>right box</DivTwo>Inside main div
+      <DivTwo>
+        right box
+        <DivThree>
+          <h2>VIRTUAL OR IN-PERSON PRIVATE LESSONS</h2>
+          <h3>1-on-1 LESSONS</h3>
+          <h4>Single Lesson $65</h4>
+          <h5>5 Lesson Package $275</h5>
+          <p>
+            One-on-One lessons are a great way to have a personal experience in
+            a class of your choice. Start training like a pro with Nelly
+            Maldonado. Classes start from a basic diagnostic warm up, then dance
+            technique, steps, choreo modules and finally partner work. You get
+            to choose hours that work for you.{" "}
+          </p>
+        </DivThree>
+        <DivFour>
+          <h2>VIRTUAL OR IN-PERSON PRIVATE LESSONS</h2>
+          <h3>1-on-1 LESSONS</h3>
+          <h4>Single Lesson $65</h4>
+          <h5>5 Lesson Package $275</h5>
+          <p>
+            One-on-One lessons are a great way to have a personal experience in
+            a class of your choice. Start training like a pro with Nelly
+            Maldonado. Classes start from a basic diagnostic warm up, then dance
+            technique, steps, choreo modules and finally partner work. You get
+            to choose hours that work for you.{" "}
+          </p>
+        </DivFour>
+      </DivTwo>
     </TopDiv>
   )
 }
@@ -12,11 +40,28 @@ export default Prices
 
 const TopDiv = styled.div`
   border: 5px solid red;
+  height: 800px;
+  display: flex;
+  justify-content: space-between;
 `
 const DivOne = styled.div`
   border: 5px solid blue;
+  width: 60%;
 `
 
 const DivTwo = styled.div`
   border: 5px solid green;
+  width: 35%;
+  margin: autol;
+`
+const DivThree = styled.div`
+  border: 5px solid brown;
+  width: 100%;
+  height: 40vh;
+`
+
+const DivFour = styled.div`
+  border: 5px solid purple;
+  width: 100%;
+  height: 40vh;
 `
