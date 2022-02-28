7d97691cdcb9bd527ae0d4dbf12510ebda6a43fc
Nelson Maldonado <play2nino@gmail.com>
2022-02-27 22:48:45 -0800

    nelson-maldonado

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <Container>
+      <Link to="/">
+        <Logo img src="/front_design.png" alt="salsa_logo" />
+      </Link>
       <Link to="/prices">
         <ButtonPrice className="btn">PRIVATE CLASSES</ButtonPrice>
       </Link>
@@ -28,7 +31,7 @@ const Home = () => {
         <Button className="btn">YouTube </Button>
       </Link>
       <Link to="/ukraine">
-        <Button className="btn">Facebook </Button>
+        <Button className="btn">YouTube </Button>
       </Link>
     </Container>
   )
@@ -37,11 +40,15 @@ const Home = () => {
 export default Home
 
 const Container = styled.main`
+  @media (max-width: 500px) {
+    height: 900px;
+    background-image: url("https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80");
+  }
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
-  margin-top: 100px;
+
   flex-direction: column;
   .btn {
     margin-top: 30px;
@@ -54,6 +61,7 @@ const Container = styled.main`
   }
 `
 const Player = styled.div`
+  margin-top: 100px;
   max-width: 800px;
   min-height: 500px;
   width: 80%;
@@ -67,4 +75,7 @@ const ButtonPrice = styled.button`
   @media (min-width: 501px) {
     display: none;
   }
+`
+const Logo = styled.img`
+  height: 80px;
 `
