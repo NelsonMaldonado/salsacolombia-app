241b99dafa6aa31d563107003fd9d5860f6d305b
Nelson Maldonado <play2nino@gmail.com>
2022-02-28 00:25:36 -0800

    working on moblie UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,32 +7,34 @@ const Home = () => {
       <Link to="/">
         <Logo img src="/front_design.png" alt="salsa_logo" />
       </Link>
-      <Link to="/prices">
-        <ButtonPrice className="btn">PRIVATE CLASSES</ButtonPrice>
-      </Link>
+
       <Player>
         <iframe
           width="100%"
           height="100%"
           src="https://www.youtube.com/embed/IR1cqagIYJY"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullscreen
         ></iframe>
       </Player>
+      <Link to="/prices">
+        <ButtonPrice className="btn">PRIVATE CLASSES</ButtonPrice>
+      </Link>
       <Link to="/ukraine">
         <Button className="btn">Ukraine Event 🇺🇦 </Button>
       </Link>
-      <Link to="/ukraine">
-        <Button className="btn">TikTok </Button>
-      </Link>
-      <Link to="/ukraine">
-        <Button className="btn">YouTube </Button>
-      </Link>
-      <Link to="/ukraine">
-        <Button className="btn">YouTube </Button>
-      </Link>
+
+      <form>
+        <ButtonTikTok className="btn">TikTok</ButtonTikTok>
+
+        <ButtonYT className="btn">YouTube </ButtonYT>
+
+        <ButtonFace className="btn" formaction="http://stackoverflow.com">
+          Facebook{" "}
+        </ButtonFace>
+      </form>
     </Container>
   )
 }
@@ -43,6 +45,10 @@ const Container = styled.main`
   @media (max-width: 500px) {
     height: 900px;
     background-image: url("https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80");
+    form {
+      display: flex;
+      flex-direction: column;
+    }
   }
   display: flex;
   justify-content: center;
@@ -70,7 +76,10 @@ const Player = styled.div`
     display: none;
   }
 `
+const ButtonYT = styled.button``
+const ButtonFace = styled.button``
 const Button = styled.button``
+const ButtonTikTok = styled.button``
 const ButtonPrice = styled.button`
   @media (min-width: 501px) {
     display: none;
