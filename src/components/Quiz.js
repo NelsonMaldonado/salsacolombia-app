1492d348a9979e3cc588a12af679eac82a261e29
Nelson Maldonado <play2nino@gmail.com>
2022-03-01 18:51:08 -0800

    modifying quiz component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,17 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
 
 const Quiz = () => {
+  const [name, setName] = useState("")
+
+  const nameFunc = (event) => {
+    setName(event.target.value)
+  }
+
   return (
     <div>
       <h1>This is a quiz to check what level of salsa you have</h1>
       <form>
+        <h2> Hey {name}</h2>
         <label>
           Full Name
-          <input type="text" name="name" />
+          <input type="text" name="name" onChange={nameFunc} />
         </label>
       </form>
+      <Card></Card>
     </div>
   )
 }
 
 export default Quiz
+
+const Card = styled.div`
+  margin: 30px auto 0 auto;
+  height: 500px;
+  width: 300px;
+  border: 1px solid red;
+`
