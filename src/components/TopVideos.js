1ca9a5d7aed7adc6dc0fb36b6c5447a37e5a142f
Nelson Maldonado <play2nino@gmail.com>
2022-03-18 23:04:28 -0700

    pushing code to sync

--- src/components/TopVideos.js
+++ src/components/TopVideos.js
@@ -11,6 +11,8 @@ const TopVideos = () => {
           url="https://www.facebook.com/salsacolombia.net/videos/1053414734678602/"
           controls
         />
+        Find out if you can uplaod reels here and make upload youtube
+        Choreography with descriptions to each one
       </Container>
     </div>
   )
